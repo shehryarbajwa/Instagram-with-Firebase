73a9b44c8584a4a0a23797ee5b53210ab93d412a
Shehryarbajwa <shehryar.bajwa@gmail.com>
2018-09-10 14:46:47 -0400

    Added Textfields

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -18,6 +18,18 @@ class ViewController: UIViewController {
         return button
     }()
     
+    let emailTextField: UITextField = {
+        
+        let textfield = UITextField()
+        textfield.placeholder = "Email"
+        textfield.translatesAutoresizingMaskIntoConstraints = false
+        textfield.borderStyle = .roundedRect
+        textfield.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        return textfield
+    }()
+    
+    
+    
     
     //Adding the view to viewDidLoad and adding contraints
     override func viewDidLoad() {
@@ -28,6 +40,16 @@ class ViewController: UIViewController {
         plusPhotoButton.widthAnchor.constraint(equalToConstant: 130).isActive = true
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+        view.addSubview(emailTextField)
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(equalTo: plusPhotoButton.bottomAnchor, constant: 20),
+            emailTextField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            emailTextField.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            emailTextField.heightAnchor.constraint(equalToConstant: 50)
+            ])
+        
+        
     }
 
 }
