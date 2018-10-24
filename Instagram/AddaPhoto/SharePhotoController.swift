6081c241c220c039755cd2ef0004254cfc8f1b0f
Shehryar <shehryar.bajwa@gmail.com>
2018-10-24 02:25:41 -0400

    Added TopLayout Guide

--- Instagram/AddaPhoto/SharePhotoController.swift
+++ Instagram/AddaPhoto/SharePhotoController.swift
@@ -12,8 +12,31 @@ class SharePhotoController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor.rgb(red: 240, green: 240, blue: 240)
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(handleShare))
+        
+        setupImageandTextViews()
+        
+    }
+    
+    fileprivate func setupImageandTextViews() {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        
+        view.addSubview(containerView)
+        
+        //TopLayoutGuide.bottomAnchor makes the view go all the way touching the top. We use the topanchor to be the bottomAnchor of the topLayout guide
+        containerView.anchor(top: topLayoutGuide.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 100)
+    }
+    
+    
+    @objc func handleShare(){
+        print("Sharing photo")
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
     
     
