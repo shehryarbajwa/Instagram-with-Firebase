98b347799a28592c9013c0ee7d9ecc279b34f0fb
Shehryar <shehryar.bajwa@gmail.com>
2018-11-06 01:22:04 -0500

    Changed searchBar color

--- Instagram/Search/UserSearchController.swift
+++ Instagram/Search/UserSearchController.swift
@@ -12,10 +12,24 @@ import UIKit
 
 class UserSearchController : UICollectionViewController {
     
+    
+    let searchBar : UISearchBar = {
+        let sb = UISearchBar()
+        sb.placeholder = "Enter username"
+        sb.barTintColor = .gray
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 240, blue: 240)
+        return sb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .blue
+        collectionView?.backgroundColor = .white
+        navigationController?.navigationBar.addSubview(searchBar)
+        
+        let navBar = navigationController?.navigationBar
+        
+        searchBar.anchor(top: navBar?.topAnchor, left: navBar?.leftAnchor, bottom: navBar?.bottomAnchor, right: navBar?.rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
     }
     
 }
