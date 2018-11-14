8ee7bd2fbb389fa1c28c844fee028681c78ec689
Shehryar <shehryar.bajwa@gmail.com>
2018-11-14 01:37:38 -0500

    Added camera feature finished

--- Instagram/Camera/CameraController.swift
+++ Instagram/Camera/CameraController.swift
@@ -11,13 +11,47 @@ import AVFoundation
 
 class Cameracontroller : UIViewController {
     
+    let dismissButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "right_arrow_shadow"), for: .normal)
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
+    
+    let capturePhotoButton : UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "capture_photo"), for: .normal)
+        button.addTarget(self, action: #selector(handleCapturePhoto), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .green
+        view.backgroundColor = .white
         
         setupCaptureSession()
+        
+        setupHUD()
+        
     }
+    
+    fileprivate func setupHUD(){
+    capturePhotoButton.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 12, paddingRight: 0, width: 80, height: 80)
+    capturePhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    
+    view.addSubview(dismissButton)
+    dismissButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 12, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 50)
+    }
+    
+    @objc func handleDismiss(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func handleCapturePhoto(){
+        print("Capturing photo")
+    }
+    
     
     fileprivate func setupCaptureSession(){
         let captureSession = AVCaptureSession()
@@ -25,6 +59,8 @@ class Cameracontroller : UIViewController {
         //The AVCApture Session, use to coordinate the flow of data from AV input devices to output
         
         //1- Setup inputs
+        //Use the AVCaptureSession() function. Then use the AVCaptureDevicve.default(for: .video/photos)
+        //Then use a do catch. It is recommended that we wrap any functions that throws (with try) into a do-catch block
         //Video capturing device on the phone
         let captureDevice = AVCaptureDevice.default(for: .video)
         
@@ -41,14 +77,18 @@ class Cameracontroller : UIViewController {
         }
         
         //2-Setup Outputs
+        let output = AVCapturePhotoOutput()
+        if captureSession.canAddOutput(output){
+            captureSession.addOutput(output)
+        }
+        
         //3-Set up preview
         
-        
-        
-        
-        
-        
-        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+        print("Successfully showcasing camera")
+        captureSession.startRunning()
     }
     
     
