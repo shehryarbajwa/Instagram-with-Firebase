61cb04789e4053fac2339909d268d72b7e7976e5
Shehryar <shehryar.bajwa@gmail.com>
2018-11-16 01:14:26 -0500

    Added save and cancel buttons

--- Instagram/Camera/CameraController.swift
+++ Instagram/Camera/CameraController.swift
@@ -79,6 +79,7 @@ class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
         let previewImage = UIImage(data: imageData!)
         
         let containerView = PreviewPhotoContainerView()
+        containerView.previewImageView.image = previewImage
         view.addSubview(containerView)
         containerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
--- Instagram/Camera/PreviewPhotoContainerView.swift
+++ Instagram/Camera/PreviewPhotoContainerView.swift
@@ -10,10 +10,48 @@ import Foundation
 import UIKit
 
 class PreviewPhotoContainerView : UIView {
+    
+    let previewImageView : UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    
+    let cancelButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "cancel_shadow"), for: .normal)
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
+    }()
+    
+    let saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "save_shadow"), for: .normal)
+        button.addTarget(self, action: #selector(handleSave), for: .touchUpInside)
+        return button
+    }()
+    
+    
     //Init frame initializes the view with a frame of CGREct properties
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .yellow
+        
+        addSubview(previewImageView)
+        previewImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        cancelButton.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        
+        
+    }
+    
+    @objc func handleCancel(){
+        
+    }
+    
+    @objc func handleSave(){
+        
     }
     
     
