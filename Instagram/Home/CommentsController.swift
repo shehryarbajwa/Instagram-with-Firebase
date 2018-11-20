073110a049aa19d03e176181c551aa0762d61b2a
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2018-11-19 20:36:39 -0500

    Added CommentsField

--- Instagram/Home/CommentsController.swift
+++ Instagram/Home/CommentsController.swift
@@ -18,7 +18,13 @@ class CommentsController : UICollectionViewController{
         
     }
     
-    var containerView : UIView = {
+    let commentTextField : UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter comment"
+        return textField
+    }()
+    
+    lazy var containerView : UIView = {
         let containerView = UIView()
         containerView.backgroundColor = .white
         containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
@@ -29,14 +35,13 @@ class CommentsController : UICollectionViewController{
         submitButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         submitButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
         
-        let textField = UITextField()
-        textField.placeholder = "Enter Comment"
-        containerView.addSubview(textField)
+        
+        containerView.addSubview(commentTextField)
         containerView.addSubview(submitButton)
         
         submitButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 0)
         
-        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 20, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+         commentTextField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitButton.leftAnchor, paddingTop: 0, paddingLeft: 20, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
         return containerView
     }()
@@ -58,8 +63,11 @@ class CommentsController : UICollectionViewController{
         }
     }
     
+    
+    
     @objc func handleSubmit(){
-        print("Handle submit")
+        guard let commenttext = commentTextField.text else {return}
+        print("Printing comment from :\(commenttext)")
     }
     
     override var canBecomeFirstResponder: Bool {
